09d5a3fe69fa9013e785750f4ff89bbf3dd7437d
universityigi <45046366+universityigi@users.noreply.github.com>
2018-12-10 11:20:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,141 @@
-$('.form').find('input').on('keyup blur focus', function (e) {
+function controllaMail() {
+  var email = document.myform.email.value;
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if (!reg.test(email)){
+    alert("La mail inserita deve essere corretta");
+    return false; 
+  }
+  else return true;
+}
+
+function controllaRipPassword() {
+  var pass= document.myform.password.value;
+  var rippass = document.myform.rippassword.value;
+  if(pass == rippass) return true;
+  else{
+    alert("Le due password non coincidono");
+    return false;
+  }
+}
+
+function controllaNome(){
+  re = /[0-9]/;
+    if(re.test(document.myform.nome.value)) {
+      alert("Il nome non può contenere numeri!");
+      document.myform.password.focus();
+      return false;
+    }
+    return true;
+}
+
+function controllaCognome(){
+  re = /[0-9]/;
+    if(re.test(document.myform.cognome.value)) {
+      alert("Il cognome non può contenere numeri!");
+      document.myform.password.focus();
+      return false;
+    }
+    return true;
+}
+
+function controllaData() {
+  if (document.myform.data.value.substring(2,3) != "/" ||
+    document.myform.data.value.substring(5,6) != "/" ||
+    isNaN(document.myform.data.value.substring(0,2)) ||
+    isNaN(document.myform.data.value.substring(3,5)) ||
+    isNaN(document.myform.data.value.substring(6,10))) {
+	alert("Inserire nascita in formato gg/mm/aaaa");
+	document.myform.data.value = "";
+	document.myform.data.focus();
+	return false;
+} else if (document.myform.data.value.substring(0,2) > 31) {
+   alert("Impossibile utilizzare un valore superiore a 31 per i giorni");
+   document.myform.data.select();
+   return false;
+} else if (document.myform.data.value.substring(3,5) > 12) {
+   alert("Impossibile utilizzare un valore superiore a 12 per i mesi");
+   document.myform.data.value = "";
+   document.myform.data.focus();
+   return false;
+} else if (document.myform.data.value.substring(6,10) < 1900) {
+   alert("Impossibile utilizzare un valore inferiore a 1900 per l'anno");
+   document.myform.data.value = "";
+   document.myform.data.focus();
+   return false;
+}
+else return true;
+}
+
+function controllaUsername(){
+  re = /^\w+$/;
+  if(!re.test(document.myform.username.value)) {
+    alert("L'username può contenere solo lettere, numeri e underscores!");
+    form.username.focus();
+    return false;
+  }
+  return true;
+}
+
+function controllaPassword(){
+    if(document.myform.password.value.length < 6) {
+      alert("La password deve contenere almeno 6 caratteri!");
+      document.myform.password.focus();
+      return false;
+    }
+    if(document.myform.password.value == document.myform.username.value) {
+      alert("La password deve essere diversa dall'username!");
+      document.myform.password.focus();
+      return false;
+    }
+    re = /[0-9]/;
+    if(!re.test(document.myform.password.value)) {
+      alert("La password deve contenere almeno un numero da 0 a 9!");
+      document.myform.password.focus();
+      return false;
+    }
+    re = /[a-z]/;
+    if(!re.test(document.myform.password.value)) {
+      alert("La password deve contenere almeno un carattere minuscolo a-z!");
+      document.myform.password.focus();
+      return false;
+    }
+    re = /[A-Z]/;
+    if(!re.test(document.myform.password.value)) {
+      alert("La password deve contenere almeno un carattere maiuscolo!");
+      document.myform.password.focus();
+      return false;
+    }
+  alert("Hai inserito una password valida: " + document.myform.password.value);
+  return true;
+}
+
+
+
+$('.form').find('input').on('keyup blur focus', function (e) {    ///INUTILE???
   
   var $this = $(this),
       label = $this.prev('label');
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
+      if (e.type === 'keyup') {
+            if ($this.val() === '') {
           label.removeClass('active highlight');
         } else {
           label.addClass('active highlight');
         }
     } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
+        if( $this.val() === '' ) {
+            label.removeClass('active highlight'); 
+            } else {
+            label.removeClass('highlight');   
+            }   
     } else if (e.type === 'focus') {
       
       if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
+            label.removeClass('highlight'); 
+            } 
       else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
+            label.addClass('highlight');
+            }
     }
 
 });
@@ -41,3 +154,4 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
   
 });
+
